27de5d3c93a3e2811040c00c31b7cbca79d327bd
wandl-6A72h <hiwepy@gmail.com>
2026-07-31 20:51:54 +0800

    refactor: groupId 改为 io.github.easy4j，url/scm 统一为 easy-4-java

--- src/main/java/org/springframework/data/redis/connection/MessageListenerAdapter.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.data.redis.connection;
-
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-
-public interface MessageListenerAdapter extends MessageListener {
-
-    void setMessageListenerContainer(RedisMessageListenerContainer container);
-
-}
